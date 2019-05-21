4667a59c6b52880b54fbac0645fcb76e401e7b34
mahmoud <hesoka_mxx@yahoo.com>
2019-05-21 06:11:31 +0300

    local settings

--- learningPlatform/settings.py
+++ learningPlatform/settings.py
@@ -135,3 +135,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
+try:
+    from .local_settings import*
+except ImportError:
+    pass
